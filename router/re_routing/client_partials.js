ab1d510c198d83deecf890fc6a7ded5f834fa17f
Aileen <aileengg938@gmail.com>
2023-11-16 04:05:36 +0800

    added

--- router/re_routing/client_partials.js
+++ router/re_routing/client_partials.js
@@ -6,67 +6,68 @@ client_partials.use(bodyParser.urlencoded({ extended: true }));
 const fs = require("node:fs");
 
 client_partials.post('/layer', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/layer.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/layer.html", "utf-8");
+    console.log(__dirname  + "/../../../pages/client_partials/layer.html")
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/home_content', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/home_content.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/home_content.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/about_content', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/about_content.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/about_content.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/brand_content', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/brands_content.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/brands_content.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/categories_content', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/categories_content.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/categories_content.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/log-in', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/log-in.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/log-in.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/msg', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/msg.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/msg.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/my-bookings', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/my_bookings.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/my_bookings.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/product', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/product.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/product.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/profile', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/profile.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/profile.html", "utf-8");
     res.json({template: html});
     res.end();
 });
 
 client_partials.post('/sign-up', (req, res) => {
-    var html = fs.readFileSync(__dirname + "../../../pages/client_partials/sign-up.html", "utf-8");
+    var html = fs.readFileSync(__dirname + "/../../../pages/client_partials/sign-up.html", "utf-8");
     res.json({template: html});
     res.end();
 });
