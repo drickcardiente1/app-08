1a9d9c3477dd6fde6690179f5a8ea82b15039eaf
Aileen <aileengg938@gmail.com>
2023-11-16 00:29:02 +0800

    reconfig

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const session = require('express-session');
-
-
 const http = require("http").createServer(app);
 const io = require("socket.io")(http)
-
 io.on('connection', socket =>{
     socket.on('msg-recieve',msg=>{
         socket.broadcast.emit('msg-send', msg );
 
     });
 })
-
 const PORT = process.env.PORT || 5001
-
 app.use(session({
   secret: 'webslesson',
   resave: true,
@@ -26,14 +21,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 
-
-
 app.get('/', (req, res) => res.render('pages/index'))
-
-
-
-
-
-
 
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
