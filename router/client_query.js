00198f99bcb516287d61bf4c3d2067a2c256bded
drickcardiente1 <95717018+drickcardiente1@users.noreply.github.com>
2023-11-29 12:18:53 +0800

    b.end

--- router/client_query.js
+++ router/client_query.js
@@ -630,7 +630,6 @@ client_queries.post('/send-msg', (req, res) => {
     }
 });
 
-
 // client_queries.post('/book', (req, res) => {
 //     if (req.session.logged_in) {
 //         qry = `INSERT INTO messages(id, sender, messages, status) VALUES ('', '${req.session.user_id}', '${req.body.message}', '1' )`;
@@ -674,9 +673,23 @@ client_queries.post('/book',upload.single('license'), (req, res) => {
                 qry = `INSERT INTO rent_list(id, client_id, bike_id, date_start, date_end, rent_days, amount_received, amount_topay, balance, status, date_created, date_updated) VALUES ('', '${req.session.user_id}', '${req.body.bike_id}', '${req.body.start}', '${req.body.end}', '${Difference_In_Days}', '0', '${price}', '${price}', '0', '${today}', '')`;
                 await promise_query(qry)
             })().then(() => {
-                console.log("job done")
-                res.sendStatus(202)
-                res.end();
+                qry = `INSERT INTO messages(id, receiver, messages, status) VALUES ('', '${req.session.user_id}', '${req.body.message}', '1' )`;
+                (async () => {
+                    await new Promise((resolve, reject) => {
+                        db.query(qry, (err, data) => {
+                            if (err) {
+                                reject(res.send({ code: 404 }))
+                            } else {
+                                resolve(data)
+                            };
+                        })
+                    }).then(data => {
+                        res.sendStatus(202)
+                        res.end();
+                    }).catch(rs => {
+                        console.log("Error such table found ", rs);
+                    })
+                })();
             }).catch(() => {
                 console.log("error 2")
                 res.sendStatus(404)
