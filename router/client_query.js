8e0b97a3e433b15cc308acfd1f982e91725a8e9d
drickcardiente1 <95717018+drickcardiente1@users.noreply.github.com>
2023-11-30 22:48:50 +0800

    s

--- router/client_query.js
+++ router/client_query.js
@@ -631,6 +631,33 @@ client_queries.post('/send-msg', (req, res) => {
     }
 });
 
+
+// client_queries.post('/send-img',upload.array('img-message'), (req, res) => {
+//     var example = ['hello world', 'hi', 'hello']
+//     if (req.session.logged_in) {
+//         qry = `INSERT INTO messages(id, sender, messages, images, status) VALUES ('', '${req.session.user_id}', '${req.body.message}','${JSON.stringify(example)}' , '1' )`;
+//         (async () => {
+//             await new Promise((resolve, reject) => {
+//                 db.query(qry, (err, data) => {
+//                     if (err) {
+//                         reject(res.send({ code: 404 }))
+//                     } else {
+//                         resolve(data)
+//                     };
+//                 })
+//             }).then(data => {
+//                 res.json({ status: 202 });
+//                 res.end();
+//             }).catch(rs => {
+//                 console.log("Error such table found ", rs);
+//             })
+//         })();
+//     } else {
+//         res.json({ status: 404 });
+//         res.end();
+//     }
+// });
+
 client_queries.post('/book',upload.single('license'), (req, res) => {
     console.log(req.body.start)
     var date1 = new Date(req.body.start);
