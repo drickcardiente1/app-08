b0b213e7e35295694f12b0af35d7d63bf1741567
drickcardiente1 <95717018+drickcardiente1@users.noreply.github.com>
2023-11-30 23:58:46 +0800

    e

--- router/client_query.js
+++ router/client_query.js
@@ -632,7 +632,7 @@ client_queries.post('/send-msg', (req, res) => {
 });
 
 
-client_queries.post('/send-img', upload.array('img_message', 10), (req, res) => {
+client_queries.post('/send-img', (req, res) => {
     if (req.session.logged_in) {
         res.json({ status: 202 });
         res.end();
