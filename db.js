7ea9e05e5e9f4968c6d538175eb7542639b31c88
Aliyah0209 <106786076+Aliyah0209@users.noreply.github.com>
2023-12-02 02:05:08 +0800

    mistake message

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-// var db = mysql.createPool({
-//   host: process.env.host,
-//   user: process.env.user,
-//   password: process.env.password,
-//   database: process.env.database,
-//   port: process.env.port,
-// });
-
 var db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "freedb_4357461_mbrdb",
-  port: "3306",
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port,
 });
+
+// var db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "freedb_4357461_mbrdb",
+//   port: "3306",
+// });
 
 db.getConnection((error) => {
   if (error) {
